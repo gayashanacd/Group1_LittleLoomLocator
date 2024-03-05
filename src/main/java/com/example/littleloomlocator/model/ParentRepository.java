c9fe1320a35ad873969069650270bdd1003d23d3
SUJEEWA <fernandow3@student.douglascollege.ca>
2024-03-04 20:08:59 -0800

    Added parent API resources

--- src/main/java/com/example/littleloomlocator/model/ParentRepository.java
+++ src/main/java/com/example/littleloomlocator/model/ParentRepository.java
@@ -9,7 +9,13 @@ public interface ParentRepository extends JpaRepository<Parent, Long> {
 
 	List<Parent> findByLastName(String lastName);
 	
+	List<Parent> findByLastNameContainingIgnoreCase(String lastName);
+	
 	List<Parent> findByFirstName(String firstName);
+	
+	List<Parent> findByFirstNameContainingIgnoreCase(String firstName);
+	
+	List<Parent> findByFirstNameContainingIgnoreCaseAndLastNameContainingIgnoreCase(String title, String code);
 	
 	List<Parent> findByEmail(String email);
 	
